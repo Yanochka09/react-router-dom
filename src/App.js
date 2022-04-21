456cc0113aaff494f217f6689fe3db72576e526f
Yanochka <zavgorodnyaya-yana@mail.ru>
2022-04-21 16:07:45 +0300

    Изменила коды в папках, которые находятся в папке src

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Main from './Main/Main';
 import Catalogue from './Catalogue/Catalogue';
 import About from './About/About';
 import Contacts from './Contacts/Contacts';
+import Menu from './Menu/Menu';
 
 function App() {
   return (
@@ -19,6 +20,11 @@ function App() {
           <Link to="/" className='Header__Link'>
             <div>
               Home
+            </div>
+          </Link>
+          <Link to="/menu/*" className='Header__Link'>
+            <div>
+              Menu
             </div>
           </Link>
           <Link to="/catalogue" className='Header__Link'>
@@ -39,6 +45,7 @@ function App() {
         </div>
         <Routes>
           <Route exact path='/' element={<Main />} />
+          <Route exact path='/menu/*' element={<Menu />} />
           <Route exact path='/catalogue/*' element={<Catalogue />} />
           <Route path='/about' element={<About />} />
           <Route path='/contacts/*' element={<Contacts />} />
---
+++ src/Menu/Menu.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Menu() {
+    return (
+        <h1>Menu list</h1>
+    )
+}
+
+export default Menu
